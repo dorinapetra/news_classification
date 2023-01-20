8a8c332b278a2d5d61429670bfb73c3beb6f4a62
dorinapetra <dorinapetra@gmail.com>
2023-01-20 14:16:46 +0100

    fix normalize year

--- news_classification/train_2.py
+++ news_classification/train_2.py
@@ -107,11 +107,11 @@ def main(config_file):
     dev_y2 = dataset['validation']['year']
     test_y = dataset['test']['domain']
 
-    train_y2 -= train_y2.min(1, keepdim=True)[0]
-    train_y2 /= train_y2.max(1, keepdim=True)[0]
+    train_y2 -= train_y2.min()
+    train_y2 /= train_y2.max()
 
-    dev_y2 -= train_y2.min(1, keepdim=True)[0]
-    dev_y2 /= train_y2.max(1, keepdim=True)[0]
+    dev_y2 -= train_y2.min()
+    dev_y2 /= train_y2.max()
 
 
 
