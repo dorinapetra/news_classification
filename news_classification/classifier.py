943576358c4f067e192b7b6ce1684840bf78a994
bart <b.botond25@gmail.com>
2023-01-20 16:42:42 +0200

    more layers

--- news_classification/classifier.py
+++ news_classification/classifier.py
@@ -11,13 +11,22 @@ class SimpleClassifier(nn.Module):
         super().__init__()
         self.input_layer = nn.Linear(input_dim, hidden_dim)
         self.relu = nn.ReLU()
-        self.output_layer = nn.Linear(hidden_dim, output_dim)
         self.drop_out = nn.Dropout(dropout_value)
+
+        self.hidden_layer = nn.Linear(hidden_dim, hidden_dim)
+        self.relu2 = nn.ReLU()
+        self.drop_out2 = nn.Dropout(dropout_value)
+        self.output_layer = nn.Linear(hidden_dim, output_dim)
+
 
     def forward(self, X):
         h = self.input_layer(X)
         h = self.relu(h)
         h = self.drop_out(h)
+
+        h = self.hidden_layer(h)
+        h = self.relu2(h)
+        h = self.drop_out2(h)
         out = self.output_layer(h)
         return out
 
