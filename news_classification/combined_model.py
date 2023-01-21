add new layer, fix normalization

news_classification/combined_model.py
@@ -13,11 +13,13 @@ class CombinedModel(torch.nn.Module):
     def __init__(self, n_hidden, n_class_output):
         super(CombinedModel, self).__init__()
         self.hidden = torch.nn.Linear(768, n_hidden)  # hidden layer
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden)  # hidden layer
         self.out = torch.nn.Linear(n_hidden, n_class_output)  # classification
         self.out2 = torch.nn.Linear(n_hidden, 1)  # regression
 
     def forward(self, x):
         x = nn.ReLU()(self.hidden(x))  # activation function for hidden layer
+        x = nn.ReLU()(self.hidden2(x))  # activation function for hidden layer
         x_out = F.softmax(self.out(x))
         x_out2 = self.out2(x)
         return x_out, x_out2
@@ -114,7 +116,7 @@ class CombinedModel(torch.nn.Module):
 
         test_out_1, test_out_2 = self.forward(test_X)
         test_loss_1 = loss_func(test_out_1, test_y1)
-        test_loss_2 = loss_func(test_out_2, test_y2)
+        test_loss_2 = loss_func(test_out_2, test_y2.unsqueeze(1))
         test_loss = test_loss_1 + test_loss_2
         test_pred = test_out_1.max(axis=1)[1]
         test_acc = float(torch.eq(test_pred, test_y1).sum().float() / len(test_X))